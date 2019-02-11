2b2e8da7a50dfe391d23185d4d1030fb8e3c5e85
Brian Haferkamp <brian.haferkamp@gmail.com>
2019-02-11 09:21:05 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -19,6 +19,22 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('activate', event => {
+  // delete any caches that aren't in expectedCaches
+  // which will get rid of static-v1
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (!cacheName.includes(key)) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      console.log(cacheName + ' now ready to handle fetches!');
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
   console.log(event.request.url);
 
